d4ae312b240d13513bddc71354192a3d766ee6ff
PabloT-7378 <nellyantonio7378@gmail.com>
2023-07-01 19:30:21 -0300

    front del proyecto
    archivos html y js, mas configuraciones y pruebas.

---
+++ proyectoBack/js/vehiculos.js
@@ -0,0 +1,70 @@
+const { createApp } = Vue
+  createApp({
+    data() {
+      return {
+        productos:[],
+        url:'http://localhost:5000/productos', 
+        error:false,
+        cargando:true,
+        /*atributos para el guardar los valores del formulario */
+        id:0,
+        marca:"",
+        modelo:"", 
+        imagen:"",
+        stock:0,
+        precio:0,
+    }  
+    },
+    methods: {
+        fetchData(url) {
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    this.productos = data;
+                    this.cargando=false
+                })
+                .catch(err => {
+                    console.error(err);
+                    this.error=true              
+                })
+        },
+        eliminar(producto) {
+            const url = this.url+'/' + producto;
+            var options = {
+                method: 'DELETE',
+            }
+            fetch(url, options)
+                .then(res => res.text()) // or res.json()
+                .then(res => {
+                    location.reload();
+                })
+        },
+        grabar(){
+            let producto = {
+                marca: this.marca,
+                modelo: this.modelo,
+                precio: this.precio,
+                stock: this.stock,
+                imagen: this.imagen
+            }
+            var options = {
+                body:JSON.stringify(producto),
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                redirect: 'follow'
+            }
+            fetch(this.url, options)
+                .then(function () {
+                    alert("Registro Guardado")
+                    window.location.href = "./vehiculos.html";  
+                })
+                .catch(err => {
+                    console.error(err);
+                    alert("Error al Guardar")
+                })      
+        }
+    },
+    created() {
+        this.fetchData(this.url)
+    },
+  }).mount('#app')
